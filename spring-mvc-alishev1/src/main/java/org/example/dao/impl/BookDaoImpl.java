07fa00cb26adf910cbc2a29eb009cdfcb00a67ab
LeraSobolevskaya <lera.sobolevskaya@bk.ru>
2022-07-08 21:48:03 +0300

    jdbcTemplate

--- spring-mvc-alishev1/src/main/java/org/example/dao/impl/BookDaoImpl.java
+++ spring-mvc-alishev1/src/main/java/org/example/dao/impl/BookDaoImpl.java
@@ -2,109 +2,55 @@ package org.example.dao.impl;
 
 import org.example.dao.BookDao;
 import org.example.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class BookDaoImpl implements BookDao {
 
     public static int COUNT;
-    private static final String URL = "jdbc:postgresql://localhost:5432/books";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "root";
-    private static final String DRIVER = "org.postgresql.Driver";
 
-    private static Connection connection;
+    public final JdbcTemplate jdbcTemplate;
 
-    static {
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    public BookDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-
 
     @Override
     public List<Book> list() {
-        List<Book> books = new ArrayList<>();
         String sql = "SELECT * FROM book";
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                Book book = new Book();
-                book.setId(resultSet.getInt("id"));
-                book.setTitle(resultSet.getString("title"));
-                book.setAuthor(resultSet.getString("author"));
-                books.add(book);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return books;
+
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class));
     }
 
     @Override
     public Book getById(int id) {
         String sql = "SELECT * FROM book WHERE id=?";
-        Book book = new Book();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                book.setId(resultSet.getInt("id"));
-                book.setTitle(resultSet.getString("title"));
-                book.setAuthor(resultSet.getString("author"));
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return book;
+
+        return jdbcTemplate.query(sql, new Object[]{id}, new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
     }
 
     @Override
     public void create(Book book) {
-        String sql = "INSERT INTO book VALUES (?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, ++COUNT);
-            preparedStatement.setString(2, book.getTitle());
-            preparedStatement.setString(3, book.getAuthor());
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        String sql = "INSERT INTO book VALUES (1, ?, ?)";
+        jdbcTemplate.update(sql, book.getTitle(), book.getAuthor());
 
     }
 
     @Override
     public void update(int id, Book book) {
         String sql = "UPDATE book SET title = ?, author = ? WHERE id=?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, book.getTitle());
-            preparedStatement.setString(2, book.getAuthor());
-            preparedStatement.setInt(3, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        jdbcTemplate.update(sql, book.getTitle(), book.getAuthor(), id);
     }
 
     @Override
     public void delete(int id) {
         String sql = "DELETE FROM book WHERE id=?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        jdbcTemplate.update(sql, id);
     }
 }
